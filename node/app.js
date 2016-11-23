f98d0a6784cf0f4cced0e5a1f5c3d866ee05ccf7
crisdiab <cristian89lara@gmail.com>
2016-11-23 14:35:41 -0500

    publish de npm

--- node/app.js
+++ node/app.js
@@ -1,4 +1,23 @@
-var numero1 = 1,
-    numero2 =2;
+tecnologiasweblarac.imprimirSuma1y2();
 
-console.log(numero1 + numero2);
+
+
+
+module.exports = {
+    
+    
+    imprimirSuma1y2 : imprimirSuma1y2,
+    
+}
+
+
+
+function imprimirSuma1y2(){
+    
+    
+    var numero1 = 1,
+        numero2 =2;
+
+    console.log(numero1 + numero2);
+    
+}
