c206500c03dcb256ba69b9cb6270ce297edb9672
Cristian Lara <cristian89lara@gmail.com>
2017-03-03 06:25:22 -0500

    crud mascotas y raza

--- Mascotas/api/controllers/RazaController.js
+++ Mascotas/api/controllers/RazaController.js
@@ -6,6 +6,41 @@
  */
 
 module.exports = {
-	
-};
 
+  crearRaza: function (req, res) {
+    var parametros = req.allParams();
+
+      if (parametros.nombre) {
+        Raza.create({
+          nombre: parametros.nombre,
+        }).exec(function (error, razaCreado) {
+          if (error) return res.view('error', {
+            title: 'Error',
+            error: {
+              descripcion: 'Hubo Problemas creando la raza, intentalo de nuevo: '+error,
+              url: '/crearRaza'
+            }
+          });
+          Raza.find().exec(function (error, razasEncontrados) {
+            if (error)
+            {
+              return res.serverError();
+            }
+            sails.log.info(razasEncontrados);
+            return res.view('vistas/Raza/listarRazas', {
+              razas: razasEncontrados
+            })
+          });
+        });
+      } else {
+        return res.view('error', {
+          title: 'Error',
+          error: {
+            descripcion: 'No envia todos los parametros',
+            url: '/crearRaza'
+          }
+        });
+      }
+
+  },
+};
--- Mascotas/config/policies.js
+++ Mascotas/config/policies.js
@@ -35,17 +35,37 @@ module.exports.policies = {
   *                                                                          *
   ***************************************************************************/
 	// RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
+    //
+	// 	Apply the `false` policy as the default for all of RabbitController's actions
+	// 	(`false` prevents all access, which ensures that nothing bad happens to our rabbits)
+	// 	'*': false,
+    //
+	// 	For the action `nurture`, apply the 'isRabbitMother' policy
+	// 	(this overrides `false` above)
+	// 	nurture	: 'isRabbitMother',
+    //
+	// 	Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
+	// 	before letting any users feed our rabbits
+	// 	feed : ['isNiceToAnimals', 'hasRabbitFood']
 	// }
+
+  RutasController: {
+
+  	// Apply the `false` policy as the default for all of RabbitController's actions
+  	// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
+  	// '*': false,
+      //
+  	// For the action `nurture`, apply the 'isRabbitMother' policy
+  	// (this overrides `false` above)
+  	// nurture	: 'isRabbitMother',
+      //
+  	// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
+  	// before letting any users feed our rabbits
+  	home: true,
+    error:true,
+listarUsuario:['autenticado'],
+    crearUsuario:['autenticado'],
+    editarUsuario:['autenticado'],
+
+  }
 };
